d23354ceaf9dbf30fefb36f4d0fc99b2abc8a7a0
Alexx <d5enemy@gmail.com>
2018-04-01 01:55:37 +0300

    changes are made in scripts

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -5,31 +5,21 @@
 # 2 >&1 redirects the standard error stream
 # to the standard output stream (stderr = 2, stdout = 1).
 #
-exec 1 > /dev/null 2 >&1
 
-#Go to the work directory
-workdirectory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#Install NTP
+apt-get update >> /dev/null 2>&1 && apt-get install -y ntp >> /dev/null 2>&1
 
-#Check the installed NTP
-if [ $(dpkg-query --show ntp 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-  apt-get install ntp -y;
-fi
-
-#Edit .conf file with back up
-sed --in-place=.backup 's/0.ubuntu.pool.ntp.org/ua.pool.ntp.org/g' /etc/ntp.conf
-sed --in-place=.backup '/.ubuntu.pool.ntp.org/d' /etc/ntp.conf
+#Edit .config NTP
+sed -i '/ntp_verify.sh/d' /etc/crontab
+sed -i '/ua.pool.ntp.org/d' /etc/ntp.conf
+sed -i 's/^pool/#pool/g' /etc/ntp.conf
+sed -i 's/^server/#server/g' /etc/ntp.conf
+sed -i '/pool 3./a \pool ua.pool.ntp.org iburst' /etc/ntp.conf
+if  [ -f /etc/ntp.conf.bak ]; then rm /etc/ntp.conf.bak; fi
+cp /etc/ntp.conf /etc/ntp.conf.bak
 
 #Restart NTP
 service ntp restart
 
-#Verify cron
-SCR="$workdirectory/ntp_verify.sh"
-JOB="*/1 * * * * $SCR MAILTO=root@localhost"
-TMPC="mycron"
-grep "$SCR" -q <(crontab -l) || (crontab -l>"$TMPC"; echo "$JOB">>"$TMPC"; crontab "$TMPC")
-rm mycron
-
-#Back up NTP .conf
-cat /etc/ntp.conf > /etc/ntp.conf.bak
-
-exit 0
+#Add to cron
+echo "*/1 *	* * *   root    $(pwd)/ntp_verify.sh	# NTP Service" >> /etc/crontab
--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
-#Go to working directory
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-DIRECTORY="$(pwd)"
+#Restart if not run
+if [ ! -n  "$(service ntp status | grep -w active)" ]; then `service ntp restart`; echo 'NOTICE: ntp is not running'; fi
 
-# Check ntpd running
-VNTP=$(ps ax | grep ntpd | wc -l)
-if [ ${VNTP} -le 1 ]
-then
-        echo "NOTICE: ntp is not running"
-        echo "Starting the ntp service..."
-        $DIRECTORY/ntp_deploy.sh
-        if [ $? -eq 0 ]
-        then
-                echo "ntp service is run"
-        fi
-fi
 
-# Check ntp .conf
-VDIFF=$(diff -u /etc/ntp.conf /etc/ntp.conf.backup | wc -l)
-if [ ${VDIFF} -gt 0 ]; then
-        echo "NOTICE: /etc/ntp.conf was changed. Calculated diff:"
-        DIFF=$(diff -u /etc/ntp.conf /etc/ntp.conf.backup | grep -v "^\ #")
-        echo -e "$DIFF"
-        cp /etc/ntp.conf.backup /etc/ntp.conf
-        service ntp restart
-fi;
+#Check .conf file
+if [ ! -f  "/etc/ntp.conf" -a ! -f  "/etc/ntp.conf.bak" ]; then echo 'NOTICE: ups...restore ntp service'
+(apt-get -o Dpkg::Options::="--force-confmiss" install --reinstall ntp) >> /dev/null 2>&1 && bash ./ntp_deploy.sh; exit $?; fi
+if [ ! -f  "/etc/ntp.conf.bak" ]; then cp /etc/ntp.conf /etc/ntp.conf.bak; echo 'NOTICE: bak file restore'; fi
+if [ ! -f  "/etc/ntp.conf" ]; then cp /etc/ntp.conf.bak /etc/ntp.conf; echo 'NOTICE: conf file restore'; fi
 
-exit 0
+#Check changes
+if [ -n "$(diff /etc/ntp.conf.bak /etc/ntp.conf)" ]; then 
+echo 'NOTICE: /etc/ntp.conf was changed. Calculated diff:'
+diff -U0  /etc/ntp.conf /etc/ntp.conf.bak
+cp /etc/ntp.conf.bak /etc/ntp.conf 
+service ntp restart; fi
