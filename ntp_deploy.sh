acedf4bb618b1a281888d40bf740086aeffab1cb
Alexx <d5enemy@gmail.com>
2018-04-01 20:09:09 +0300

    latest updates with .gitignore and deleting readme.md

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-#
-# >/dev/null redirects the command standard output to the null device, 
-# which is a special device which discards the information written to it. 
-# 2 >&1 redirects the standard error stream
-# to the standard output stream (stderr = 2, stdout = 1).
-#
 
-#Install NTP
 apt-get update >> /dev/null 2>&1 && apt-get install -y ntp >> /dev/null 2>&1
 
-#Edit .config NTP
 sed -i '/ntp_verify.sh/d' /etc/crontab
 sed -i '/ua.pool.ntp.org/d' /etc/ntp.conf
 sed -i 's/^pool/#pool/g' /etc/ntp.conf
 sed -i 's/^server/#server/g' /etc/ntp.conf
 sed -i '/pool 3./a \pool ua.pool.ntp.org iburst' /etc/ntp.conf
-if  [ -f /etc/ntp.conf.bak ]; then rm /etc/ntp.conf.bak; fi
+if  [ -f /etc/ntp.conf.bak ]; then rm -rf /etc/ntp.conf.bak; fi
 cp /etc/ntp.conf /etc/ntp.conf.bak
 
-#Restart NTP
 service ntp restart
 
-#Add to cron
 echo "*/1 *	* * *   root    $(pwd)/ntp_verify.sh	# NTP Service" >> /etc/crontab
--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-#Restart if not run
 if [ ! -n  "$(service ntp status | grep -w active)" ]; then `service ntp restart`; echo 'NOTICE: ntp is not running'; fi
 
-
-#Check .conf file
 if [ ! -f  "/etc/ntp.conf" -a ! -f  "/etc/ntp.conf.bak" ]; then echo 'NOTICE: ups...restore ntp service'
 (apt-get -o Dpkg::Options::="--force-confmiss" install --reinstall ntp) >> /dev/null 2>&1 && bash ./ntp_deploy.sh; exit $?; fi
 if [ ! -f  "/etc/ntp.conf.bak" ]; then cp /etc/ntp.conf /etc/ntp.conf.bak; echo 'NOTICE: bak file restore'; fi
 if [ ! -f  "/etc/ntp.conf" ]; then cp /etc/ntp.conf.bak /etc/ntp.conf; echo 'NOTICE: conf file restore'; fi
 
-#Check changes
 if [ -n "$(diff /etc/ntp.conf.bak /etc/ntp.conf)" ]; then 
 echo 'NOTICE: /etc/ntp.conf was changed. Calculated diff:'
 diff -U0  /etc/ntp.conf /etc/ntp.conf.bak
